346b7ad450167a5e4e6988c05fc6db8dac39557c
Murphy Stevens <murphyjstevens@protonmail.com>
2024-02-10 18:10:50 -0600

    Removed unnecessary parameter

--- components/user-story.vue
+++ components/user-story.vue
@@ -1,21 +1,48 @@
 <template>
-  <Dialog v-model:visible="displayDialog" :maximizable="true" :modal="true" :style="{width: '80vw', minHeight: '50vh', maxHeight: '90vh'}">
+  <Dialog
+    v-model:visible="displayDialog"
+    :maximizable="true"
+    :modal="true"
+    :style="{width: '80vw', minHeight: '50vh', maxHeight: '90vh'}"
+  >
     <template #header>
       <div class="flex items-center grow">
         <i class="pi pi-book text-2xl text-primary-300 me-2" v-tooltip.top="'User Story'"/>
-        <InputText type="text" v-model="title" placeholder="Insert title here" class="!bg-transparent !border-transparent text-lg grow hover:!border-inherit" />
+        <InputText
+          type="text"
+          v-model="title"
+          placeholder="Insert title here"
+          class="!bg-transparent !border-transparent text-lg grow hover:!border-inherit"
+        />
       </div>
     </template>
     <div class="flex flex-col grow">
-      <Panel header="Details" toggleable class="mb-2">
-        <Dropdown v-model="selectedStatus" :options="statuses" optionLabel="display" placeholder="Select the Status" />
+      <Panel
+        header="Details"
+        toggleable
+        class="mb-2"
+      >
+        <Dropdown
+          v-model="selectedStatus"
+          :options="statuses"
+          optionLabel="display"
+          placeholder="Select the Status"
+        />
       </Panel>
       <Panel header="Description" toggleable>
-        <Textarea v-model="description" rows="5" resize="none" />
+        <Textarea
+          v-model="description"
+          rows="5"
+          resize="none"
+        />
       </Panel>
     </div>
     <template #footer>
-      <Button label="Save" class="ms-2" @click="save" />
+      <Button
+        label="Save"
+        class="ms-2"
+        @click="save"
+      />
     </template>
   </Dialog>
 </template>
@@ -23,27 +50,24 @@
 <script setup lang="ts">
 import { type Ref, ref } from 'vue'
 
-defineProps<{
-  header?: string
-}>()
-
 defineExpose({
   show
 })
 
+const header: Ref<string> = ref('')
 const title: Ref<string> = ref('')
 const description: Ref<string> = ref('')
 const selectedStatus: Ref<any> = ref(null)
 const statuses: Ref<any[]> = ref([
   { id: 0, display: 'New'},
   { id: 1, display: 'Pending'},
-  { id: 2, display: 'Paused'},
   { id: 3, display: 'Done'},
   { id: 4, display: 'Removed'}
 ])
 const displayDialog: Ref<boolean> = ref(false)
 
-function show() {
+function show(dialogHeader: string) {
+  header.value = dialogHeader
   displayDialog.value = true
 }
 
--- pages/backlog.vue
+++ pages/backlog.vue
@@ -12,22 +12,61 @@
       >
         <template #header>
           <div class="flex justify-end">
-            <SplitButton label="User Story" icon="pi pi-plus" @click="openUserStoryDialog()" :model="addDropDownItems"/>
+            <SplitButton
+              label="User Story"
+              icon="pi pi-plus"
+              @click="openUserStoryDialog()"
+              :model="addDropDownItems"
+            />
           </div>
         </template>
-        <Column field="priority" header="Priority" style="width: 100px;" sortable></Column>
-        <Column field="title" header="Title"></Column>
-        <Column field="type" header="Type" style="width: 150px;" sortable></Column>
-        <Column header="" style="width: 100px;" class="text-center">
+        <Column
+          field="priority"
+          header="Priority"
+          style="width: 100px;"
+          sortable
+        ></Column>
+        <Column
+          field="title"
+          header="Title"
+        ></Column>
+        <Column
+          field="type"
+          header="Type"
+          style="width: 150px;"
+          sortable
+        ></Column>
+        <Column
+          header=""
+          style="width: 100px;"
+          class="text-center"
+        >
             <template #body="item">
-              <Button icon="pi pi-pencil" size="small" text rounded class="w-8 h-8 !p-0 me-2" @click="edit(item.data)" />
-              <Button icon="pi pi-trash" severity="danger" size="small" text rounded class="w-8 h-8 !p-0" @click="confirmDelete(item.data)" />
+              <Button
+                icon="pi pi-pencil"
+                aria-label="Edit Backlog Item"
+                size="small"
+                text
+                rounded
+                class="w-8 h-8 !p-0 me-2"
+                @click="edit(item.data)"
+              />
+              <Button
+                icon="pi pi-trash"
+                aria-label="Delete Backlog Item"
+                severity="danger"
+                size="small"
+                text
+                rounded
+                class="w-8 h-8 !p-0"
+                @click="confirmDelete(item.data)"
+              />
             </template>
         </Column>
       </DataTable>
     </div>
     
-    <UserStory ref="userStoryDialog" :header=userStoryDialogHeader />
+    <UserStory ref="userStoryDialog" />
   </div>
 </template>
 
@@ -36,35 +75,49 @@ import { type Ref, ref } from 'vue'
 import type { BacklogItem } from '~/models'
 import { BacklogItemType } from '~/models/enums'
 
+const userStoryDialog: Ref<any> = ref()
+
 const items: Ref<BacklogItem[]> = ref([
-  { priority: 40, title: 'Dickerson', type: BacklogItemType.Bug } as BacklogItem,
-  { priority: 21, title: 'Larsen', type: BacklogItemType.UserStory } as BacklogItem,
-  { priority: 89, title: 'Geneva', type: BacklogItemType.Bug } as BacklogItem,
-  { priority: 38, title: 'Jami', type: BacklogItemType.UserStory } as BacklogItem
+  { 
+    priority: 40,
+    title: 'Dickerson',
+    type: BacklogItemType.Bug
+  } as BacklogItem,
+  {
+    priority: 21,
+    title: 'Larsen',
+    type: BacklogItemType.UserStory
+  } as BacklogItem,
+  {
+    priority: 89,
+    title: 'Geneva',
+    type: BacklogItemType.Bug
+  } as BacklogItem,
+  {
+    priority: 38,
+    title: 'Jami',
+    type: BacklogItemType.UserStory
+  } as BacklogItem
 ])
 
 const addDropDownItems: Ref<any[]> = ref([
   {
     label: 'Add Bug',
-    icon: 'fas fa-plus',
+    icon: 'pi pi-plus',
     command: () => {
       openBugDialog()
     }
   }
 ])
 
-const userStoryDialogHeader: Ref<string> = ref('')
-
-const userStoryDialog: Ref<any> = ref()
-
 function openUserStoryDialog() {
-  userStoryDialogHeader.value = 'Add User Story'
-  userStoryDialog.value.show()
+  const dialogHeader: string = 'Add User Story'
+  userStoryDialog.value.show(dialogHeader)
 }
 
 function openBugDialog() {
-  userStoryDialogHeader.value = 'Add Bug'
-  userStoryDialog.value.show()
+  const dialogHeader: string = 'Add Bug'
+  userStoryDialog.value.show(dialogHeader)
 }
 
 function edit(item: any) {
